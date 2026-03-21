a961ffcb313747cca242d457edaf22f39633ac0e
L1nf3ng <nuaa_llf@126.com>
2026-03-22 01:48:11 +0800

    拆分系统提示词和用户提问。

--- src/ai.rs
+++ src/ai.rs
@@ -1,14 +1,14 @@
 use async_openai::{Client, config::OpenAIConfig};
 use async_openai::types::chat::{
-    CreateChatCompletionRequestArgs,
-    ChatCompletionRequestUserMessageArgs,
-    ChatCompletionRequestMessage,
+    ChatCompletionRequestMessage, ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs
 };
 use std::error::Error;
 
 const API_KEY: &str = "sk-api-OhrNszaS2-4Y7GwxfKF0G-7mw-9Gf5tBamMvVKSeigenBZRMJ7S9h1t2P87si1MnKMwqvpl627oNAD1KJlfOznoUQuw1icjcUtKasaeWIUw6MI2Bqe00XYc";
 const BASE_URL: &str = "https://api.minimaxi.com/v1";
 const MODEL_NAME: &str = "MiniMax-M2.7";
+const SYSTEM_PROMPT: &str  = "你是一个白盒专家，我通过codeql、semgrep等获得了一些sarif报告，但我没有时间去验证报告里漏洞的真伪。我会给你污点传播链路以及所涉及的相关源码，你需要帮我验证它们的真伪，同时评估漏洞的重要等级，并提供修复建议。"
+
 
 pub async fn get_a_client() -> Client<OpenAIConfig>{
     let config = OpenAIConfig::new()
@@ -19,13 +19,19 @@ pub async fn get_a_client() -> Client<OpenAIConfig>{
 
 
 pub async fn chat_with_model(client: Client<OpenAIConfig>, message: &str ) -> Result<Option<String>,Box<dyn Error>>{
+    let message_args = ChatCompletionRequestUserMessageArgs::default()
+        .content(message)
+        .build()?;
+    let prompt_args = ChatCompletionRequestSystemMessageArgs::default()
+        .content(SYSTEM_PROMPT)
+        .build()?;
+    
     let request = CreateChatCompletionRequestArgs::default()
         .model(MODEL_NAME)
-        .messages(vec![ChatCompletionRequestMessage::User(
-            ChatCompletionRequestUserMessageArgs::default()
-                .content(message)
-                .build()?
-        )])
+        .messages(vec![
+            ChatCompletionRequestMessage::System(prompt_args),
+            ChatCompletionRequestMessage::User(message_args)
+        ])
         .max_tokens(1000u32)
         .build()?;
 
