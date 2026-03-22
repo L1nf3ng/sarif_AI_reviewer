e8474c735dca94b6ef6ecfe95a81a2e8a17db034
L1nf3ng <nuaa_llf@126.com>
2026-03-22 16:21:59 +0800

    删除不必要的ApiKey，避免泄露

--- src/ai_chat.rs
+++ src/ai_chat.rs
@@ -3,22 +3,25 @@ use async_openai::types::chat::{
     ChatCompletionRequestMessage, ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs
 };
 use std::error::Error;
+use std::env;
+use dotenv::dotenv;
 
-const API_KEY: &str = "sk-api-OhrNszaS2-4Y7GwxfKF0G-7mw-9Gf5tBamMvVKSeigenBZRMJ7S9h1t2P87si1MnKMwqvpl627oNAD1KJlfOznoUQuw1icjcUtKasaeWIUw6MI2Bqe00XYc";
-const BASE_URL: &str = "https://api.minimaxi.com/v1";
-const MODEL_NAME: &str = "MiniMax-M2.7";
 const SYSTEM_PROMPT: &str  = "你是一个白盒专家，我通过codeql、semgrep等获得了一些sarif报告，但我没有时间去验证报告里漏洞的真伪。我会给你污点传播链路以及所涉及的相关源码，你需要帮我验证它们的真伪，同时评估漏洞的重要等级，并提供修复建议。";
 
 
 pub async fn get_a_client() -> Client<OpenAIConfig>{
+    dotenv().ok();
+    let api_key = env::var("API_KEY").expect("LLM APIKEY must be set!");
+    let base_url = env::var("BASE_URL").expect("Set A OpenAI Compatible Endpoint for your LLM!");
     let config = OpenAIConfig::new()
-        .with_api_key(API_KEY)
-        .with_api_base(BASE_URL);
+        .with_api_key(api_key)
+        .with_api_base(base_url);
     Client::with_config(config)
 }
 
 
 pub async fn chat_with_model(client: Client<OpenAIConfig>, message: &str ) -> Result<Option<String>,Box<dyn Error>>{
+    let model_name = env::var("MODEL_NAME").expect("You must set a model name!");
     let message_args = ChatCompletionRequestUserMessageArgs::default()
         .content(message)
         .build()?;
@@ -27,7 +30,7 @@ pub async fn chat_with_model(client: Client<OpenAIConfig>, message: &str ) -> Re
         .build()?;
     
     let request = CreateChatCompletionRequestArgs::default()
-        .model(MODEL_NAME)
+        .model(model_name)
         .messages(vec![
             ChatCompletionRequestMessage::System(prompt_args),
             ChatCompletionRequestMessage::User(message_args)
